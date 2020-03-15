e7673f492f9e532c3d2f4154ca4027eab5dfeb86
Mukta Girkar <muktagirkar@gmail.com>
2020-03-15 01:36:35 -0700

    Create Leetcode_20.ValidParentheses.py

---
+++ Leetcode_20.ValidParentheses.py
@@ -0,0 +1,39 @@
+def StrCheck(string):
+    a=list(string)
+    
+    stc=list()
+    
+    i=0
+    top=-1 
+    
+    for i in range (0, len(a)):
+        
+        # print(a[i])
+        
+        if ( a[i]== "{" or a[i]== "(" or a[i]== "[" ):
+            
+            # print ("in")
+            stc.append(a[i])
+            
+            top+=1
+        
+        else:
+            
+            if((a[i]=="}" and stc[top]== "{") or
+               (a[i]==")" and stc[top]== "(") or
+               (a[i]=="]" and stc[top]== "[") ):
+                
+                top-=1
+            
+            else:
+                return False
+    
+    if(top==-1):
+        return True
+    else:
+        return False
+    
+if (__name__=="__main__"):
+    print(StrCheck("{[]}"))
+    
+    
