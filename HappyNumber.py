4a7e2882d65f177268222799b29ac83291451c46
Mukta Girkar <muktagirkar@gmail.com>
2020-04-03 00:13:05 -0700

    Update HappyNumber.py

--- HappyNumber.py
+++ HappyNumber.py
@@ -1,16 +1,15 @@
-def HappyNumber(num):
+class Solution:
+    def isHappy(self, num: int) -> bool:
 
-    while(len(list(map(int, str(num))))>1):
-        l= list(map(int, str(num)))
-        sum=0
-        for i in range (len(l)):
-            sum=sum+(l[i]*l[i])
-        num=sum
-    # print (num)
-    if(num==1):
-        return 1
-    else:
-        return 0
+        while(1):
 
-if (__name__=="__main__"):
-    print(HappyNumber(68))
+            l= list(map(int, str(num)))
+            sum=0
+            for i in range (len(l)):
+                sum=sum+(l[i]*l[i])
+            num=sum
+
+            if(num==1):
+                return 1
+            elif(num==4):
+                return 0
