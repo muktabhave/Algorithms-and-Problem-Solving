b069a41e91eee41cd16a4ca11aa8f05e2cf40216
Mukta Girkar <muktagirkar@gmail.com>
2020-04-09 23:33:30 -0700

    Update CountEle.py

--- CountEle.py
+++ CountEle.py
@@ -1,7 +1,7 @@
-/*
 
+# Given an integer array arr, count element x such that x + 1 is also in arr.
 
-*/
+# If there're duplicates in arr, count them seperately.
 
 def CountEle(a):
     nums=dict()
