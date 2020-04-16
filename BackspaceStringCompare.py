d92e0031ee9a280499ecba6e34b74ea2b55259a1
Mukta Girkar <muktagirkar@gmail.com>
2020-04-16 16:26:04 -0700

    Create BackspaceStringCompare.py

---
+++ BackspaceStringCompare.py
@@ -0,0 +1,29 @@
+def BackspaceStringCompare(c1,c2):
+    c1=list(c1)
+    c2=list(c2)
+    stc1=[]
+    stc2=[]
+    
+    i=0
+    while(i< len(c1)):
+        if(c1[i]=="#" and len(stc1)>0):
+            stc1.pop()
+        else:
+            stc1.append(c1[i])
+        i+=1
+    j=0
+    while(j<len(c2)):
+        if(c2[j]=="#" and len(stc2)>0):
+            stc2.pop()
+        else:
+            stc2.append(c2[j])
+        j+=1        
+    
+    if(stc1==stc2):
+        return True
+    else:
+        return False    
+    
+    
+if(__name__=="__main__"):
+    print(BackspaceStringCompare('ab#d','aec##d'))
