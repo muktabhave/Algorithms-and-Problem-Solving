97543cd5a0f469ca137d6a62cb69b66bf491abfb
Mukta Girkar <muktagirkar@gmail.com>
2020-04-16 17:23:14 -0700

    Update MinStack.py

--- MinStack.py
+++ MinStack.py
@@ -5,10 +5,10 @@
 # top() -- Get the top element.
 # getMin() -- Retrieve the minimum element in the stack.
 
-class Stack:
+class MinStack:
 
     def __init__(self):
-        self.top=-1
+        self.topele=-1
         self.min=-1
         self.items=[]
         
@@ -16,8 +16,8 @@ class Stack:
     def push(self, x: int) -> None:
         
         self.items.append(x)
-        self.top+=1
-        if(self.top==0):
+        self.topele+=1
+        if(self.topele==0):
             self.min=x
         else:
             if(x<self.min):
@@ -25,34 +25,21 @@ class Stack:
         
 
     def pop(self) -> None:
-        if(self.min==self.items[self.top]):
+        if(self.topele==-1):
+            return
+        if(self.min==self.items[self.topele]):
             self.min=self.items[0]
-            for j in range (1,self.top-1):
+            for j in range (0,self.topele):
                 if (self.items[j]<self.min):
                     self.min= self.items[j]
             
         
         self.items.pop()
-        self.top-=1
+        self.topele-=1
         
 
     def top(self) -> int:
-        return self.top    
+        return self.items[self.topele]    
     
     def getMin(self) -> int:
         return self.min
-            
-    
-if (__name__=="__main__"):
-    
-    s=Stack()
-    
-    s.push(10)
-    s.push(5)
-    s.push(20)
-    s.push(30)
-    s.pop()
-    s.pop()
-    s.pop()
-    print (s.items)
-    print(s.getMin())
