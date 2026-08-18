1eb52c9caf0fafbbfd13f6d493fb38f30577a5f6
Mukta Girkar <muktagirkar@gmail.com>
2026-08-18 15:54:15 -0700

    Update ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -3,6 +3,32 @@
 Trick is how you convert number to list and list back to number
 */
 
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+ANS:
+
 class Solution:
     def reverse(self, x: int) -> int:
       
